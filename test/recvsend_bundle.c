3c7b4857f2f4be0a343108b01ea9e74336c6ce2f
Sebastian Chlad <sebastian.chlad@suse.com>
2025-08-13 22:06:30 +0200

    s

--- test/recvsend_bundle.c
+++ test/recvsend_bundle.c
@@ -748,6 +748,7 @@ int main(int argc, char *argv[])
 	if (argc > 1)
 		return T_EXIT_SKIP;
 
+	classic_buffers = 1;
 	ret = test_tcp();
 	if (ret != T_EXIT_PASS)
 		return ret;
@@ -756,7 +757,7 @@ int main(int argc, char *argv[])
 	if (ret != T_EXIT_PASS)
 		return ret;
 
-	classic_buffers = 1;
+	classic_buffers = 0;
 
 	ret = test_tcp();
 	if (ret != T_EXIT_PASS)
