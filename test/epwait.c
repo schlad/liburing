f59c329186ff0b09e45c47fbfce1a65aa7046434
Sebastian Chlad <sebastian.chlad@suse.com>
2025-10-15 21:52:59 +0200

    s

--- test/epwait.c
+++ test/epwait.c
@@ -16,8 +16,12 @@
 #include <time.h>
 #include <unistd.h>
 
-static _Atomic unsigned long wr_ticks = 0;    /* writer loop iterations */
-static _Atomic unsigned long wr_bytes = 0;    /* total bytes written    */
+static _Atomic unsigned long wr_ticks = 0;   /* incremented in writer */
+static _Atomic unsigned long wr_bytes = 0;   /* incremented in writer */
+
+static _Atomic int wd_run = 0;               /* watchdog running flag */
+static _Atomic int wd_waiting = 0;           /* main is in wait() */
+static int wd_efd = -1;                      /* epoll fd for probe */
 
 static int fds[2][2];
 static int no_epoll_wait;
@@ -213,12 +217,52 @@ static void *thread_fn(void *data)
         usleep(150);
         for (int i = 0; i < NPIPES; i++) {
             int ret = write(d->pipes[i][1], "foo", 3);
-            if (ret < 0)
-                perror("write");
-            else
+            if (ret >= 0)
                 atomic_fetch_add(&wr_bytes, (unsigned long)ret);
+            else
+                perror("write");
         }
         atomic_fetch_add(&wr_ticks, 1UL);
+    }
+    return NULL;
+}
+
+static void *watchdog_fn(void *arg)
+{
+    (void)arg;
+    unsigned long last_ticks = 0, last_bytes = 0;
+    struct timespec start = {0}, now = {0};
+    int have_window = 0;
+
+    while (atomic_load(&wd_run)) {
+        if (atomic_load(&wd_waiting)) {
+            unsigned long t = atomic_load(&wr_ticks);
+            unsigned long b = atomic_load(&wr_bytes);
+            if (!have_window) {
+                last_ticks = t; last_bytes = b;
+                clock_gettime(CLOCK_MONOTONIC, &start);
+                have_window = 1;
+            } else {
+                clock_gettime(CLOCK_MONOTONIC, &now);
+                long ms = (now.tv_sec - start.tv_sec) * 1000L +
+                          (now.tv_nsec - start.tv_nsec) / 1000000L;
+                if (ms >= 1000 && t == last_ticks && b == last_bytes) {
+                    /* probe epoll readiness without blocking */
+                    struct epoll_event probe[NPIPES];
+                    int n = epoll_wait(wd_efd, probe, NPIPES, 0);
+                    long ncpu = sysconf(_SC_NPROCESSORS_ONLN);
+                    fprintf(stderr,
+                        "STARVATION(BLOCKED): waited ~%ld ms, writer unchanged "
+                        "(ticks=%lu, bytes=%lu), epoll n=%d, CPUs=%ld\n",
+                        ms, t, b, n, ncpu);
+                    /* reset window so we don’t spam */
+                    have_window = 0;
+                }
+            }
+        } else {
+            have_window = 0; /* not waiting now */
+        }
+        usleep(500 * 1000); /* 500 ms */
     }
     return NULL;
 }
@@ -243,151 +287,132 @@ static inline long diff_ms(struct timespec a, struct timespec b)
 
 static int test_race(int flags)
 {
-	struct io_uring ring;
-	struct io_uring_sqe *sqe;
-	struct io_uring_cqe *cqe;
-	struct d d;
-	struct epoll_event ev;
-	/* two ping-pong buffers */
-	struct epoll_event out[2][NPIPES];
-	pthread_t thread;
-	int i, j, efd, ret;
-	void *tret;
+    struct io_uring ring;
+    struct io_uring_sqe *sqe;
+    struct io_uring_cqe *cqe;
+    struct d d;
+    struct epoll_event ev;
+    struct epoll_event out[2][NPIPES];
+    pthread_t thread, wd;
+    int i, j, efd, ret;
+    void *tret;
 
-	ret = t_create_ring(32, &ring, flags);
-	if (ret == T_SETUP_SKIP)
-		return 0;
-	else if (ret != T_SETUP_OK) {
-		fprintf(stderr, "ring create failed %x -> %d\n", flags, ret);
-		return 1;
-	}
+    ret = t_create_ring(32, &ring, flags);
+    if (ret == T_SETUP_SKIP) return 0;
+    if (ret != T_SETUP_OK) {
+        fprintf(stderr, "ring create failed %x -> %d\n", flags, ret);
+        return 1;
+    }
 
-	for (i = 0; i < NPIPES; i++) {
-		if (pipe(d.pipes[i]) < 0) {
-			perror("pipe");
-			io_uring_queue_exit(&ring);
-			return 1;
-		}
-	}
+    for (i = 0; i < NPIPES; i++) {
+        if (pipe(d.pipes[i]) < 0) {
+            perror("pipe");
+            io_uring_queue_exit(&ring);
+            return 1;
+        }
+    }
 
-	efd = epoll_create1(0);
-	if (efd < 0) {
-		perror("epoll_create");
-		for (i = 0; i < NPIPES; i++) { close(d.pipes[i][0]); close(d.pipes[i][1]); }
-		io_uring_queue_exit(&ring);
-		return T_EXIT_FAIL;
-	}
+    efd = epoll_create1(0);
+    if (efd < 0) {
+        perror("epoll_create");
+        for (i = 0; i < NPIPES; i++) { close(d.pipes[i][0]); close(d.pipes[i][1]); }
+        io_uring_queue_exit(&ring);
+        return T_EXIT_FAIL;
+    }
+    wd_efd = efd; /* watchdog will probe this fd */
 
-	for (i = 0; i < NPIPES; i++) {
-		ev.events = EPOLLIN;
-		ev.data.fd = d.pipes[i][0];
-		ret = epoll_ctl(efd, EPOLL_CTL_ADD, d.pipes[i][0], &ev);
-		if (ret < 0) {
-			perror("epoll_ctl");
-			for (i = 0; i < NPIPES; i++) { close(d.pipes[i][0]); close(d.pipes[i][1]); }
-			close(efd);
-			io_uring_queue_exit(&ring);
-			return T_EXIT_FAIL;
-		}
-	}
+    for (i = 0; i < NPIPES; i++) {
+        ev.events = EPOLLIN;
+        ev.data.fd = d.pipes[i][0];
+        ret = epoll_ctl(efd, EPOLL_CTL_ADD, d.pipes[i][0], &ev);
+        if (ret < 0) {
+            perror("epoll_ctl");
+            for (i = 0; i < NPIPES; i++) { close(d.pipes[i][0]); close(d.pipes[i][1]); }
+            close(efd);
+            io_uring_queue_exit(&ring);
+            return T_EXIT_FAIL;
+        }
+    }
 
-	memset(out, 0, sizeof(out));
+    memset(out, 0, sizeof(out));
 
-	/* arm TWO waits up front; tag them with user_data = 0 / 1 */
-	sqe = io_uring_get_sqe(&ring);
-	io_uring_prep_epoll_wait(sqe, efd, out[0], NPIPES, 0);
-	sqe->user_data = 0;
+    /* arm TWO waits up front; tag user_data = 0 / 1 */
+    sqe = io_uring_get_sqe(&ring);
+    io_uring_prep_epoll_wait(sqe, efd, out[0], NPIPES, 0);
+    sqe->user_data = 0;
 
-	sqe = io_uring_get_sqe(&ring);
-	io_uring_prep_epoll_wait(sqe, efd, out[1], NPIPES, 0);
-	sqe->user_data = 1;
+    sqe = io_uring_get_sqe(&ring);
+    io_uring_prep_epoll_wait(sqe, efd, out[1], NPIPES, 0);
+    sqe->user_data = 1;
 
-	io_uring_submit(&ring);
+    io_uring_submit(&ring);
 
-	/* start writer */
-	pthread_create(&thread, NULL, thread_fn, &d);
+    /* start writer + watchdog */
+    pthread_create(&thread, NULL, thread_fn, &d);
+    atomic_store(&wd_run, 1);
+    pthread_create(&wd, NULL, watchdog_fn, NULL);
 
-	for (j = 0; j < LOOPS; j++) {
-		/* ---- starvation probe: snapshot BEFORE blocking ---- */
-		unsigned long t0_ticks = atomic_load(&wr_ticks);
-		unsigned long b0       = atomic_load(&wr_bytes);
-		struct timespec ts0, ts1;
-		clock_gettime(CLOCK_MONOTONIC, &ts0);
+    for (j = 0; j < LOOPS; j++) {
+        atomic_store(&wd_waiting, 1);
+        ret = io_uring_wait_cqe(&ring, &cqe);
+        atomic_store(&wd_waiting, 0);
+        if (ret) {
+            fprintf(stderr, "wait %d\n", ret);
+            goto fail;
+        }
+        if (cqe->res < 0) {
+            fprintf(stderr, "race res %d\n", cqe->res);
+            io_uring_cqe_seen(&ring, cqe);
+            goto fail;
+        }
 
-		ret = io_uring_wait_cqe(&ring, &cqe);
+        /* which buffer got filled */
+        unsigned buf_id = (unsigned)cqe->user_data;
+        if (buf_id > 1) buf_id = 0;
 
-		clock_gettime(CLOCK_MONOTONIC, &ts1);
-		unsigned long t1_ticks = atomic_load(&wr_ticks);
-		unsigned long b1       = atomic_load(&wr_bytes);
-		long waited_ms         = diff_ms(ts0, ts1);
+        /* drain exactly cqe->res */
+        for (i = 0; i < cqe->res; i++) {
+            char tmp[32];
+            int fd = out[buf_id][i].data.fd;
+            if (fd >= 0) {
+                int r = read(fd, tmp, sizeof(tmp));
+                if (r < 0) perror("read");
+            }
+        }
+        memset(out[buf_id], 0, sizeof(struct epoll_event) * (unsigned)cqe->res);
 
-		if (ret) {
-			fprintf(stderr, "wait %d\n", ret);
-			goto fail;
-		}
-		if (cqe->res < 0) {
-			fprintf(stderr, "race res %d\n", cqe->res);
-			io_uring_cqe_seen(&ring, cqe);
-			goto fail;
-		}
+        io_uring_cqe_seen(&ring, cqe);
 
-		/* If we blocked "long enough" and writer didn't advance at all -> starvation */
-		if (waited_ms > 500 && t1_ticks == t0_ticks && b1 == b0) {
-			struct epoll_event probe[NPIPES];
-			int n = epoll_wait(efd, probe, NPIPES, 0); /* was anything readable now? */
-			long ncpu = sysconf(_SC_NPROCESSORS_ONLN);
-			fprintf(stderr,
-			        "STARVATION? blocked %ld ms, writer unchanged "
-			        "(ticks=%lu, bytes=%lu), epoll n=%d, CPUs=%ld, flags=0x%x%s\n",
-			        waited_ms, t1_ticks, b1, n, ncpu, flags,
-			        (flags & IORING_SETUP_SQPOLL) ? " (SQPOLL)" : "");
-		}
+        /* immediately re-arm the same buffer */
+        sqe = io_uring_get_sqe(&ring);
+        io_uring_prep_epoll_wait(sqe, efd, out[buf_id], NPIPES, 0);
+        sqe->user_data = buf_id;
+        io_uring_submit(&ring);
+    }
 
-		/* identify which buffer got filled: 0 or 1 */
-		unsigned buf_id = (unsigned)cqe->user_data;
-		if (buf_id > 1) buf_id = 0; /* defensive */
+    atomic_store(&wd_run, 0);
+    pthread_join(wd, &tret);
+    pthread_join(thread, &tret);
 
-		/* drain exactly cqe->res events from the right buffer */
-		for (i = 0; i < cqe->res; i++) {
-			char tmp[32];
-			int fd = out[buf_id][i].data.fd;
-			if (fd >= 0) {
-				int r = read(fd, tmp, sizeof(tmp));
-				if (r < 0)
-					perror("read");
-			}
-		}
-		/* clear only the entries we consumed */
-		memset(out[buf_id], 0, sizeof(struct epoll_event) * (unsigned)cqe->res);
-
-		io_uring_cqe_seen(&ring, cqe);
-
-		/* immediately re-arm the SAME buffer we just consumed from */
-		sqe = io_uring_get_sqe(&ring);
-		io_uring_prep_epoll_wait(sqe, efd, out[buf_id], NPIPES, 0);
-		sqe->user_data = buf_id;
-		io_uring_submit(&ring);
-	}
-
-	pthread_join(thread, &tret);
-
-	for (i = 0; i < NPIPES; i++) {
-		close(d.pipes[i][0]);
-		close(d.pipes[i][1]);
-	}
-	close(efd);
-	io_uring_queue_exit(&ring);
-	return 0;
+    for (i = 0; i < NPIPES; i++) {
+        close(d.pipes[i][0]);
+        close(d.pipes[i][1]);
+    }
+    close(efd);
+    io_uring_queue_exit(&ring);
+    return 0;
 
 fail:
-	pthread_join(thread, &tret);
-	for (i = 0; i < NPIPES; i++) {
-		close(d.pipes[i][0]);
-		close(d.pipes[i][1]);
-	}
-	close(efd);
-	io_uring_queue_exit(&ring);
-	return 1;
+    atomic_store(&wd_run, 0);
+    pthread_join(wd, &tret);
+    pthread_join(thread, &tret);
+    for (i = 0; i < NPIPES; i++) {
+        close(d.pipes[i][0]);
+        close(d.pipes[i][1]);
+    }
+    close(efd);
+    io_uring_queue_exit(&ring);
+    return 1;
 }
 
 static int test(int flags)
