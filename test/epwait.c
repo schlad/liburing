0d40415fad9852f69b6d24d3d556c9e3c531488c
Sebastian Chlad <sebastian.chlad@suse.com>
2025-10-15 22:28:11 +0200

    kup

--- test/epwait.c
+++ test/epwait.c
@@ -456,14 +456,14 @@ static int test(int flags)
 		close(fds[i][1]);
 	}
 
-	fprintf(stderr, "[ctrl] epoll-only start\n"); fflush(stderr);
-	int ctrl = test_race_epoll_only();
-	fprintf(stderr, "[ctrl] epoll-only rc=%d\n", ctrl); fflush(stderr);
+	//fprintf(stderr, "[ctrl] epoll-only start\n"); fflush(stderr);
+	//int ctrl = test_race_epoll_only();
+	//fprintf(stderr, "[ctrl] epoll-only rc=%d\n", ctrl); fflush(stderr);
 
-	if (ctrl) {
-    	fprintf(stderr, "epoll-only control failed/hung -> not an io_uring-specific issue\n");
-    	return T_EXIT_FAIL;
-	}
+	//if (ctrl) {
+    //	fprintf(stderr, "epoll-only control failed/hung -> not an io_uring-specific issue\n");
+    //	return T_EXIT_FAIL;
+	//}
 
 	fprintf(stderr, "[ctrl] test_race start\n"); fflush(stderr);
 	ret = test_race(flags);
